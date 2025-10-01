c59aaa0c168892cae83545e3b2663a7f9cd891cd
Yugal Y. K. <140834062+Yugal-kosamshile@users.noreply.github.com>
2025-10-01 14:26:51 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import Home from './home.jsx'
 export default function App() {
 
-  let [count, setCount]=useState(0);
-
   return (
-    
     <>
     <Home/>
-    
     </>
   )
 }
